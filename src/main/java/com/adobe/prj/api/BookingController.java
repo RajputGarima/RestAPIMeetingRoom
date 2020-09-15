90f2a2eaa88a6f9034cecd80ae3a534e643371f8
Garima <gari@adobe.com>
2020-09-16 00:57:20 +0530

    Merge commit

--- src/main/java/com/adobe/prj/api/BookingController.java
+++ src/main/java/com/adobe/prj/api/BookingController.java
@@ -35,73 +35,70 @@ public class BookingController {
 	@Autowired
 	private UserService userService;
 
-	@Autowired
-	private EquipmentService equipmentService;
-	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private FoodService foodService;
-	
-	@Autowired
-	private RoomService roomService;
+//	@Autowired
+//	private EquipmentService equipmentService;
+//	
+//	@Autowired
+//	private FoodService foodService;
+//	
+//	@Autowired
+//	private RoomService roomService;
 
 	
 	@GetMapping()
     public @ResponseBody List<Booking> getBookings() {
 		
 
-		long milli = 123456789999l;
-		Booking b = new Booking();
-		b.setAttendees(10);
-
-		b.setRoom(roomService.getRoom(1));
-		b.setTotalCost(2700);
-		java.sql.Time time = new java.sql.Time(milli);
-		BookingSchedule bs = new BookingSchedule();
-		bs.setStartTime(time);
-		bs.setEndTime(time);
-
-		b.setSchedule(bs);
-		b.setUser(userService.getUser("k@adobe.com"));
-		
-		EquipmentDetail ed = new EquipmentDetail();
-		ed.setEquipment(equipmentService.getEquipment(1));
-		ed.setUnits(10);
-		ed.setPrice(ed.getUnits() * equipmentService.getEquipment(1).getPrice());
-		
-		b.getEquipDetails().add(ed);
-		
-		EquipmentDetail ed1 = new EquipmentDetail();
-		ed1.setEquipment(equipmentService.getEquipment(2));
-		ed1.setUnits(20);
-		ed1.setPrice(ed1.getUnits() * equipmentService.getEquipment(2).getPrice());
-		
-		b.getEquipDetails().add(ed1);
-		
-		FoodBooking fb = new FoodBooking();
-		fb.setFood(foodService.getFood(1));
-		fb.setQuantity(6);
-		fb.setAmount(fb.getQuantity() * foodService.getFood(1).getFoodPrice());
-		
-		b.getFoods().add(fb);
-		
-		FoodBooking fb1 = new FoodBooking();
-		fb1.setFood(foodService.getFood(2));
-		fb1.setQuantity(10);
-		fb1.setAmount(fb1.getQuantity() * foodService.getFood(2).getFoodPrice());
-		
-		b.getFoods().add(fb1);
-		
-		FoodBooking fb2 = new FoodBooking();
-		fb2.setFood(foodService.getFood(3));
-		fb2.setQuantity(5);
-		fb2.setAmount(fb2.getQuantity() * foodService.getFood(3).getFoodPrice());
-		
-		b.getFoods().add(fb2);
-		
-		bookingService.addBooking(b);
+//		long milli = 123456789999l;
+//		Booking b = new Booking();
+//		b.setAttendees(10);
+//
+//		b.setRoom(roomService.getRoom(1));
+//		b.setTotalCost(2700);
+//		java.sql.Time time = new java.sql.Time(milli);
+//		BookingSchedule bs = new BookingSchedule();
+//		bs.setStartTime(time);
+//		bs.setEndTime(time);
+//
+//		b.setSchedule(bs);
+////		b.setUser(userService.getUser("k@adobe.com"));
+//		
+//		EquipmentDetail ed = new EquipmentDetail();
+//		ed.setEquipment(equipmentService.getEquipment(1));
+//		ed.setUnits(10);
+//		ed.setPrice(ed.getUnits() * equipmentService.getEquipment(1).getPrice());
+//		
+//		b.getEquipDetails().add(ed);
+//		
+//		EquipmentDetail ed1 = new EquipmentDetail();
+//		ed1.setEquipment(equipmentService.getEquipment(2));
+//		ed1.setUnits(20);
+//		ed1.setPrice(ed1.getUnits() * equipmentService.getEquipment(2).getPrice());
+//		
+//		b.getEquipDetails().add(ed1);
+//		
+//		FoodBooking fb = new FoodBooking();
+//		fb.setFood(foodService.getFood(1));
+//		fb.setQuantity(6);
+//		fb.setAmount(fb.getQuantity() * foodService.getFood(1).getFoodPrice());
+//		
+//		b.getFoods().add(fb);
+//		
+//		FoodBooking fb1 = new FoodBooking();
+//		fb1.setFood(foodService.getFood(2));
+//		fb1.setQuantity(10);
+//		fb1.setAmount(fb1.getQuantity() * foodService.getFood(2).getFoodPrice());
+//		
+//		b.getFoods().add(fb1);
+//		
+//		FoodBooking fb2 = new FoodBooking();
+//		fb2.setFood(foodService.getFood(3));
+//		fb2.setQuantity(5);
+//		fb2.setAmount(fb2.getQuantity() * foodService.getFood(3).getFoodPrice());
+//		
+//		b.getFoods().add(fb2);
+//		
+//		bookingService.addBooking(b);
 
         return bookingService.getBookings();
     }
@@ -115,7 +112,7 @@ public class BookingController {
 	public @ResponseBody Booking addBooking(@RequestBody Booking b) {
 		User user = userService.addUser(b.getUser());
 		b.setUser(user);
-	  return bookingService.addBooking(b);
+		return bookingService.addBooking(b);
 	}
 	
 	@DeleteMapping("/{id}")
