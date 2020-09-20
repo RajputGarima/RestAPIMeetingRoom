8f2906ffc4bea4e5d29459963431e5907a65cd87
krkothar <krkothar@adobe.com>
2020-09-20 17:12:51 +0530

    room status update resolved

---
+++ src/main/java/com/adobe/prj/util/RoomStatus.java
@@ -0,0 +1,17 @@
+package com.adobe.prj.util;
+
+public class RoomStatus {
+	private boolean active;
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+
+	
+	
+}
